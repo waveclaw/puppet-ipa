bcebb0e8195da49e95f5811bbb6a753a80be7119
Jeremiah D. Powell <waveclaw@waveclaw.com>
2016-02-27 21:01:03 -0600

    tests for sssd.conf and krb5.conf

--- lib/facter/ipa_master.rb
+++ lib/facter/ipa_master.rb
@@ -14,49 +14,58 @@ EOF
   class << self
 
     def sssd_master
-      Facter::core.Execution.exec(
-        "awk '/^ipa_server = / {print $3}' /etc/sssd/sssd.conf")
+      master = nil
+      ssd_conf = File.open('/etc/sssd/sssd.conf','r') # for instrumentation
+      ssd_conf.each_line {|line|
+        if (line =~ /ipa_server\s*=\s*(.+)/)
+          master = $1.chomp
+        end
+      }
+      master
     end
 
     def ldap_master
-      uri = Facter::core.Execution.exec(
-        "awk '/^URI / {print $2}' /etc/openldap/ldap.conf")
-      if (uri =~ /ldap[s]?:\/\/(\S+)/)
-        $1
-      end
+      master = nil
+      ldap_conf = File.open('/etc/openldap/ldap.conf','r') # for instrumentation
+      ldap_conf.each_line { |line|
+        if (line =~ /^URI\s+(\S+)/)
+          master = $1.split(':')[1].split('/')[2]  # not my finest hack
+        end
+      }
+      master
     end
 
     def krb5_master
-      master_kdc = nil
-      default_realm = Facter::core.Execution.exec(
+      master = nil
+      default_realm = Facter::Util::Resolution.exec(
         "awk '/^default_realm.*=.*/ {print $NF}' /etc/krb5.conf | head -1")
       # search for master_kdc in default_realm in [realms]
       # using a brute-force stateful scanner
       if default_realm
-       begin
+       begin # file error very likely due to close bugs
         File.open('/etc/krb5.conf','r') { |line|
           realms = true if line =~ /\[realms\].*/
           realms = false if (realms and line =~ /\s*}\s*/)
           in_default = true if (realms and line =~ /#{default_realm}\s*=\s*\{.*/)
           if (in_default and line =~ /.*master_kdc\s*=\s*(\S+)/)
-             master_kdc = $1
+             master = $1
            end
          }
        Rescue FileError => fe
        end
       end
-      master_kdc
+      master
     end
 
     def ipa_master
       master = nil
       begin
         #TODO: add an ipa-client attempt, too
-        if File.exists? '/etc/sssd/sssd.conf'
+        if File.exist? '/etc/sssd/sssd.conf'
           master = self.sssd_master
-        elsif (master == nil and File.exists? '/etc/openldap/ldap.conf')
+        elsif (master == nil and File.exist? '/etc/openldap/ldap.conf')
           master = self.ldap_master
-        elsif (master == nil and File.exists? '/etc/krb5.conf')
+        elsif (master == nil and File.exist? '/etc/krb5.conf')
           master = self.krb5_master
         else
           master = nil
@@ -71,9 +80,9 @@ end
 
 Facter.add(:ipa_master) do
   confine do
-    File.exists? '/etc/sssd/sssd.conf' or
-    File.exists? '/etc/krb5.conf' or
-    File.exists? '/etc/openldap/ldap.conf'
+    File.exist? '/etc/sssd/sssd.conf' or
+    File.exist? '/etc/krb5.conf' or
+    File.exist? '/etc/openldap/ldap.conf'
   end
   setcode { Facter::Util::Ipa_master.ipa_master }
 end
--- spec/functions/ipa_master_spec.rb
+++ spec/functions/ipa_master_spec.rb
@@ -8,18 +8,154 @@
 #
 
 require 'spec_helper'
-require 'facter/rhsm_available_repos'
+require 'facter/ipa_master'
 
+ssd_examples = [
+  {
+    :desc     => 'nothing when there is no data',
+    :data     => '',
+    :expected => nil
+  },
+  {
+    :desc     => 'the whole list when there is a list of data',
+    :data     => 'ipa_server = _srv_, ipa.example.com',
+    :expected => '_srv_, ipa.example.com'
+  },
+  {
+    :desc     => 'the whole list when there is a different list of data',
+    :data     => '
+garbage
+ipa_server=ipa.example.com,_srv_
+some=moretrash
+',
+    :expected => 'ipa.example.com,_srv_'
+  },
+  {
+    :desc     => 'the whole list when there is an IP address in a list of data',
+    :data     => 'ipa_server =  1.2.3.4, freeipa',
+    :expected => '1.2.3.4, freeipa'
+  },
 
-describe Facter::Util::Rhsm_available_repos, :type => :puppet_function do
+  {
+    :desc     => 'the whole list when there is an IP address',
+    :data     => 'ipa_server =  1.2.3.4',
+    :expected => '1.2.3.4'
+  },
+
+  {
+    :desc     => 'the whole list when there is an alpha numeric host',
+    :data     => 'ipa_server =  freeipa0001',
+    :expected => 'freeipa0001'
+  }
+]
+
+ldap_examples = [
+  {
+    :desc     => 'nothing when there is no data',
+    :data     => '',
+    :expected => nil
+  },
+  {
+    :desc     => 'nothing for non-null garbage',
+    :data     => '
+some nifty garbageURI stuff
+
+',
+    :expected => nil
+  },
+  {
+    :desc     => 'an ldap portless URI',
+    :data     => '
+ipa_server =  1.2.3.4, freeipa
+URI	ldap://ldap.example.com
+stuff',
+    :expected => 'ldap.example.com'
+  },
+  {
+    :desc     => 'an ldap URI with port',
+    :data     => 'URI	ldap://ldap.example.com:389',
+    :expected => 'ldap.example.com'
+  },
+  {
+    :desc     => 'an ldaps portless URI',
+    :data     => 'URI	ldaps://ldap.example.com',
+    :expected => 'ldap.example.com'
+  },
+  {
+    :desc     => 'an ldaps URI with port',
+    :data     => 'URI	ldaps://ldap.example.com:636',
+    :expected => 'ldap.example.com'
+  },
+  {
+    :desc     => 'an ldap URI with numerical host',
+    :data     => 'URI	ldap://1.2.3.4',
+    :expected => '1.2.3.4'
+  },
+  {
+    :desc     => 'an ldap URI with numerical host with port',
+    :data     => 'URI	ldap://1.2.3.4:389',
+    :expected => '1.2.3.4'
+  }
+]
+
+describe Facter::Util::Ipa_master, :type => :puppet_function do
   context 'with just sssd.conf' do
     before :each do
       allow(File).to receive(:exist?).with(
       '/etc/sssd/sssd.conf' ) { true }
+      allow(File).to receive(:exist?).with(
+      '/etc/krb5.conf' ) { false }
+      allow(File).to receive(:exist?).with(
+      '/etc/openldap/ldap.conf' ) { false }
+
     end
     it "should return nothing when there is an error" do
+      expect(File).to receive(:open).with(
+        '/etc/sssd/sssd.conf','r') { throw Error }
+      expect(Facter::Util::Ipa_master.ipa_master).to eq(nil)
+    end
+    ssd_examples.each {|xample|
+      it "should return #{xample[:desc]}" do
+        expect(File).to receive(:open).with(
+          '/etc/sssd/sssd.conf','r') { StringIO.new(xample[:data]) }
+        expect(Facter::Util::Ipa_master.ipa_master).to eq(xample[:expected])
+      end
+    }
+  end
+  context 'with just ldap.conf' do
+    before :each do
+      allow(File).to receive(:exist?).with('/etc/sssd/sssd.conf' ) { false }
+    end
+    it "should return nothing when there is an error" do
+      expect(File).to receive(:exist?).with('/etc/openldap/ldap.conf' ) { true }
+      expect(File).to receive(:open).with(
+        '/etc/openldap/ldap.conf','r') { throw Error }
+      expect(Facter::Util::Ipa_master.ipa_master).to eq(nil)
+    end
+    ldap_examples.each {|xample|
+      it "should return a master for #{xample[:desc]}" do
+        expect(File).to receive(:exist?).with('/etc/openldap/ldap.conf' ) { true }
+        expect(File).to receive(:open).with(
+          '/etc/openldap/ldap.conf','r') { StringIO.new(xample[:data]) }
+        expect(Facter::Util::Ipa_master.ipa_master).to eq(xample[:expected])
+      end
+    }
+  end
+  context 'with just krb5.conf' do
+    before :each do
+        allow(File).to receive(:exist?).with('/etc/sssd/sssd.conf' ) { false }
+        allow(File).to receive(:exist?).with('/etc/openldap/ldap.conf' ) { false }
+    end
+    it "should return nothing when there is an error" do
+      expect(File).to receive(:exist?).with( '/etc/krb5.conf' ) { true }
       expect(Facter::Util::Resolution).to receive(:exec).with(
-        '/usr/sbin/subscription-manager repos') { throw Error }
+        "awk '/^default_realm.*=.*/ {print $NF}' /etc/krb5.conf | head -1") { throw Error }
+      expect(Facter::Util::Ipa_master.ipa_master).to eq(nil)
+    end
+    it "should return nothing when there is a no data" do
+      expect(File).to receive(:exist?).with( '/etc/krb5.conf' ) { true }
+      expect(Facter::Util::Resolution).to receive(:exec).with(
+        "awk '/^default_realm.*=.*/ {print $NF}' /etc/krb5.conf | head -1") { '' }
       expect(Facter::Util::Ipa_master.ipa_master).to eq(nil)
     end
   end
@@ -28,7 +164,7 @@ describe Facter::Util::Rhsm_available_repos, :type => :puppet_function do
       allow(File).to receive(:exist?).with(
       '/etc/sssd/sssd.conf' ) { false }
       allow(File).to receive(:exist?).with(
-      '/etc/sssd/krb5.conf' ) { false }
+      '/etc/krb5.conf' ) { false }
       allow(File).to receive(:exist?).with(
       '/etc/openldap/ldap.conf' ) { false }
     end
