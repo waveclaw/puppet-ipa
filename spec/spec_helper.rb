3ada7373459319460b1ac79036c95cb33c6c0d79
Jeremiah D. Powell <waveclaw@waveclaw.com>
2016-05-28 00:34:09 -0500

    do not die if codeclimate is not available

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,16 @@
 # First line of spec/spec_helper.rb
+begin
 require 'codeclimate-test-reporter'
-
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
-]
-
+  CodeClimate::TestReporter::Formatter ]
 SimpleCov.start do
   add_filter '/spec/'
   # Exclude bundled Gems in `/.vendor/`
   add_filter '/.vendor/'
+end
+rescue LoadError => e
+ puts e.to_s
 end
 
 require 'puppetlabs_spec_helper/module_spec_helper'
