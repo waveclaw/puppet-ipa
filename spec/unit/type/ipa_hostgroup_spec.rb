d3e7ae6f83005b55f788d5935473c1e6836681be
Jeremiah D. Powell <waveclaw@waveclaw.com>
2017-05-04 19:46:37 -0500

    add an ipa_hostgroup test

---
+++ spec/unit/type/ipa_hostgroup_spec.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -S rspec
+#
+#  Test the type interface of the ipa_group type.
+#
+#   Copyright 2016 JD Powell <waveclaw@hotmail.com>
+#
+#   See LICENSE for licensing.
+#
+require 'spec_helper'
+require 'type_spec_tests'
+
+# Example:
+#ipa_hostgroup { 'puppet_servers':
+#  ensure      => 'present',
+#  description => 'Puppet Servers',
+#}
+
+described_class = Puppet::Type.type(:ipa_group)
+
+describe described_class, 'type' do
+  it_behaves_like 'has ensurable', described_class
+  it_behaves_like 'has properties',  described_class,
+   [ :description, ]
+  it_behaves_like 'has a name', described_class
+end
